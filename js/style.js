bbe608d7deae4565ca932855ff4468f5a0ec0f3e
Line-jpg <lines1310@gmail.com>
2025-12-03 12:08:10 +0100

    Kode fra tidligere opgave indsat

--- js/style.js
+++ js/style.js
@@ -1,1 +1,18 @@
 "use strict";
+
+const sections = document.querySelectorAll("section");
+
+// Tager fat om browservinduet, og gør noget noget ved
+window.addEventListener("scroll", () => {
+  sections.forEach((sec) => {
+    const top = window.scrollY;
+    const offset = sec.offsetTop - window.innerHeight / 1.5; // justér 1.5 for timing
+    const height = sec.offsetHeight;
+
+    if (top >= offset && top < offset + height) {
+      sec.classList.add("show-animate");
+    } else {
+      sec.classList.remove("show-animate");
+    }
+  });
+});
