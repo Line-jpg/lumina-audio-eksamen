387e9edae697f6a92b3503568a7d1ac6eaf3a1bc
Line-jpg <lines1310@gmail.com>
2025-12-05 12:07:38 +0100

    click event tilføjet på højtalerens farve knapper

--- js/style.js
+++ js/style.js
@@ -2,11 +2,10 @@
 
 const sections = document.querySelectorAll("section");
 
-// Tager fat om browservinduet, og gør noget noget ved
 window.addEventListener("scroll", () => {
   sections.forEach((sec) => {
     const top = window.scrollY;
-    const offset = sec.offsetTop - window.innerHeight / 1.5;
+    const offset = sec.offsetTop - window.innerHeight / 1.5; 
     const height = sec.offsetHeight;
 
     if (top >= offset && top < offset + height) {
@@ -16,3 +15,55 @@ window.addEventListener("scroll", () => {
     }
   });
 });
+
+const beigeKnap = document.querySelector('.farveBtn1');
+const beigeImg = new Image();
+beigeImg.src = 'img/produktImg.png';
+  
+if (beigeKnap) {
+  beigeKnap.addEventListener('click', function () {
+    const produktImg = document.getElementById('produktImg');
+    if (produktImg) {
+      produktImg.src = 'img/produktImg.png';
+    }
+  });
+}
+
+const navyKnap = document.querySelector('.farveBtn2');
+const navyImage = new Image();
+navyImage.src = 'img/navySpeaker.png';
+
+if (navyKnap) {
+  navyKnap.addEventListener('click', function () {
+    const produktImg = document.getElementById('produktImg');
+    if (produktImg) {
+      produktImg.src = 'img/navySpeaker.png';
+    }
+  });
+}
+
+const hvidKnap = document.querySelector('.farveBtn3');
+const hvidImage = new Image();
+hvidImage.src = 'img/hvidSpeaker.png';
+
+if (hvidKnap) {
+  hvidKnap.addEventListener('click', function () {
+    const produktImg = document.getElementById('produktImg');
+    if (produktImg) {
+      produktImg.src = 'img/hvidSpeaker.png';
+    }
+  });
+}
+
+const sortKnap = document.querySelector('.farveBtn4');
+const sortImage = new Image();
+sortImage.src = 'img/sortSpeaker.png';
+
+if (sortKnap) {
+  sortKnap.addEventListener('click', function () {
+    const produktImg = document.getElementById('produktImg');
+    if (produktImg) {
+      produktImg.src = 'img/sortSpeaker.png';
+    }
+  });
+}
